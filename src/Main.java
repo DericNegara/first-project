25187ab9eb593a6da115d1574e0af97d009df416
Deric <13twiink13@gmail.com>
2022-11-02 22:01:18 +0100

    .

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Level1:
+        double vasyasCash = 1000;
+        double vasyasCashAfterShopping = 1000;
+
+        boolean vasyasPurchases = (vasyasCash != vasyasCashAfterShopping);
+
+        System.out.println(vasyasPurchases);
+
+        //Level2:
+        double vasyasGPAIfHeImproves = 3.49 + 1;
+        double vasyasGPAScope = 5;
+
+        boolean vasyasGPA = (vasyasGPAIfHeImproves >= vasyasGPAScope);
+
+        System.out.println(vasyasGPA);
+
+
+
+        //>  - больше
+        //<  - меньше
+        //== - равно
+        //!= - не равно
+        //!  - отрицание
+        //>= -больше-равно
+        //<= -мкньше-равно
+
     }
 }
